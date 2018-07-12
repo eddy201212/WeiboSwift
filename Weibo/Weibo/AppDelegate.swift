e18fd3b8242c48fb117584f64312caf6d18915d4
eddy201212 <liyilin1209@163.com>
2018-07-12 14:36:51 +0800

    设置应用程序额外信息

--- Weibo/Weibo/AppDelegate.swift
+++ Weibo/Weibo/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import SVProgressHUD
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,23 +18,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
+        // 应用程序额外设置
+        setupAdditions()
+        
         window = UIWindow()
-        
         window?.backgroundColor = UIColor.white
+        
         window?.rootViewController = WBMainViewController()
-        
         window?.makeKeyAndVisible()
-        
-        test()
         
         return true
     }
 
-    func test() {
-    
-    }
-
-    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
@@ -54,7 +51,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+}
 
 
+// MARK: - 设置应用程序额外信息
+extension AppDelegate {
+    
+    fileprivate func setupAdditions() {
+        
+        // 1. 设置 SVProgressHUD 最小消失时间
+        SVProgressHUD.setMinimumDismissTimeInterval(1)
+        
+        // 2. 设置用户授权显示通知
+        // #available 是检测设备版本，如果是 10.0 以上
+        if #available(iOS 10.0, *) {
+            UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .carPlay, .sound]) { (success, error) in
+                print("授权" + (success ? "成功" : "失败"))
+            }
+        } else {
+            // 10.0 以下
+            // 取得用户授权显示通知[上方的提示条/声音/BadgeNumber]
+            let notifySettings = UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            UIApplication.shared.registerUserNotificationSettings(notifySettings)
+        }
+    }
 }
 
