be512c39d5e4d2f1479b268377a9b6c31d2d0ae6
eddy201212 <liyilin1209@163.com>
2019-08-18 15:48:41 +0800

    提交

---
+++ Weibo/Weibo/Test/MyTest.swift
@@ -0,0 +1,17 @@
+//
+//  MyTest.swift
+//  Weibo
+//
+//  Created by Eddy on 2019/8/18.
+//  Copyright © 2019年 WB. All rights reserved.
+//
+
+import Foundation
+
+class MyTest {
+    
+    func testfuc() {
+        
+        print("8899")
+    }
+}
